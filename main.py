ba321f2e16644e1c71d5c1c7fde155ba13f9980a
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-09-19 21:31:12 -0400

    added cors

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:19006",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # will receive a list containing of 3 players to validate 
 # playerNames[0] will be the player on the row
