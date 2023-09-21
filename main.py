07eae49d5e80445a39c54015ee4d83ad7c1c30f0
Anthony Valenti <57304403+AnthonyValenti@users.noreply.github.com>
2023-09-21 11:42:12 -0400

    craeteDB function added

--- main.py
+++ main.py
@@ -8,10 +8,21 @@ import random
 
 app = FastAPI()
 
+def createDB():
+    conn = sqlite3.connect('teammateGrid.db')
+    cursor = conn.cursor()
+    with open('teammateGrid.sql', 'r') as sql_file:
+        sql_script = sql_file.read()
+    cursor.executescript(sql_script)
+    conn.commit()
+    conn.close()
+
+
 class User(BaseModel):
     username: str
     password: str
 
+createDB()
 
 origins = [
     "http://localhost",
